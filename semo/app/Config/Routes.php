aec72b52b2d47d35c2914a7383f9920eb6cf26c7
Theyraa <theovanza.21122006@mahasiswa.unikom.ac.id>
2024-11-20 10:05:55 +0700

    update theeyra

--- semo/app/Config/Routes.php
+++ semo/app/Config/Routes.php
@@ -5,7 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-    $routes->get('/', 'Beranda::index');
+	$routes->get('/', 'Beranda::index');
 	$routes->get('/detail', 'Detail::index');
 	$routes->get('/cart', 'Cart::index');
 	$routes->get('/checkout', 'Checkout::index');
+	$routes->get('/dashboard', 'Dashboard::index');
